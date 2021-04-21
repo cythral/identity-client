b5bbb6f16429f2627a02ad00a803edd7774ad4b8
Talen Fisher <talen.fisher@cythral.com>
2021-04-21 18:34:47 -0500

    Fix Formatting

--- tests/Common/AutoAttribute.cs
+++ tests/Common/AutoAttribute.cs
@@ -1,15 +1,9 @@
-using System;
-using System.IO;
 using System.Linq;
 using System.Text.Json;
 
 using AutoFixture;
 using AutoFixture.AutoNSubstitute;
 using AutoFixture.NUnit3;
-
-using Microsoft.Extensions.Options;
-
-using PuppeteerSharp;
 
 internal class AutoAttribute : AutoDataAttribute
 {
