b8c9582e17ae8c38cea33a4c212385ce28fc63d4
Talen Fisher <talen.fisher@cythral.com>
2021-06-28 11:43:32 -0500

    BRGD-134 User Token Store Cache Control

--- tests/UserTokenStoreTests.cs
+++ tests/UserTokenStoreTests.cs
@@ -138,5 +138,75 @@ namespace Brighid.Identity.Client
                 await client.Received().ExchangeAccessTokenForImpersonateToken(Is(accessToken), Is(userId), Is(audience), Any<CancellationToken>());
             }
         }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class InvalidateAllUserTokens
+        {
+            [Test, Auto]
+            public async Task ShouldInvalidateTokensForAllUsers(
+                string userId1,
+                string userId2,
+                string audience,
+                string accessToken,
+                Token token,
+                [Frozen, Substitute] ITokenStore tokenStore,
+                [Frozen, Substitute] IdentityServerClient client,
+                [Target] UserTokenStore store
+            )
+            {
+                var cancellationToken = new CancellationToken();
+                tokenStore.GetToken(Any<CancellationToken>()).Returns(accessToken);
+                client.ExchangeAccessTokenForImpersonateToken(Any<string>(), Any<string>(), Any<string>(), Any<CancellationToken>()).Returns(token);
+
+                await store.GetUserToken(userId1, audience, cancellationToken);
+                await store.GetUserToken(userId2, audience, cancellationToken);
+                store.InvalidateAllUserTokens();
+                client.ClearReceivedCalls();
+
+                await store.GetUserToken(userId1, audience, cancellationToken);
+                await store.GetUserToken(userId2, audience, cancellationToken);
+
+                await client.Received().ExchangeAccessTokenForImpersonateToken(Is(accessToken), Is(userId1), Is(audience), Any<CancellationToken>());
+                await client.Received().ExchangeAccessTokenForImpersonateToken(Is(accessToken), Is(userId2), Is(audience), Any<CancellationToken>());
+            }
+        }
+
+        [TestFixture]
+        [Category("Unit")]
+        public class InvalidateUserToken
+        {
+            [Test, Auto]
+            public async Task ShouldInvalidateAllTokensForTheGivenUserId(
+                string userId1,
+                string userId2,
+                string audience1,
+                string audience2,
+                string accessToken,
+                Token token,
+                [Frozen, Substitute] ITokenStore tokenStore,
+                [Frozen, Substitute] IdentityServerClient client,
+                [Target] UserTokenStore store
+            )
+            {
+                var cancellationToken = new CancellationToken();
+                tokenStore.GetToken(Any<CancellationToken>()).Returns(accessToken);
+                client.ExchangeAccessTokenForImpersonateToken(Any<string>(), Any<string>(), Any<string>(), Any<CancellationToken>()).Returns(token);
+
+                await store.GetUserToken(userId1, audience1, cancellationToken);
+                await store.GetUserToken(userId1, audience2, cancellationToken);
+                await store.GetUserToken(userId2, audience1, cancellationToken);
+                store.InvalidateTokensForUser(userId1);
+                client.ClearReceivedCalls();
+
+                await store.GetUserToken(userId1, audience1, cancellationToken);
+                await store.GetUserToken(userId1, audience2, cancellationToken);
+                await store.GetUserToken(userId2, audience1, cancellationToken);
+
+                await client.Received().ExchangeAccessTokenForImpersonateToken(Is(accessToken), Is(userId1), Is(audience1), Any<CancellationToken>());
+                await client.Received().ExchangeAccessTokenForImpersonateToken(Is(accessToken), Is(userId1), Is(audience2), Any<CancellationToken>());
+                await client.DidNotReceive().ExchangeAccessTokenForImpersonateToken(Is(accessToken), Is(userId2), Is(audience1), Any<CancellationToken>());
+            }
+        }
     }
 }
